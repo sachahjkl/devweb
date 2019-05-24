6807d542aeee7bd7cbb2edd1c13240ec9d4e923c
sachafroment <sacha.froment@gmail.com>
2019-05-24 15:47:46 +0200

    Ajout leaflet, flags, title

--- layouts/head.php
+++ layouts/head.php
@@ -1,5 +1,5 @@
 <head>
     <meta charset="utf-8" />
     <link rel="stylesheet" href="style/style.css" />
-    <title></title>
+    <title><?=isset($title) ? $title : "Quefa : Importation de café";?></title>
 </head>
