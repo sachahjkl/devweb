0ebe12fdb183085023eb8d1229a8ed56554ee1c3
sachafroment <sacha.froment@gmail.com>
2019-05-24 20:56:08 +0200

    map

---
+++ js/map.js
@@ -0,0 +1,46 @@
+var mymap;
+const zoom = 12
+const token = "pk.eyJ1Ijoic2FjaGFmcm9tZW50IiwiYSI6ImNqcDczaTlvcDBhcHEzcG14b3RlY3JjYjUifQ.lqDhfMfAZTdS6HztmANh2A";
+
+//parametres par défaut de l'api toilettes
+const defLang = "fr";
+
+$(document).ready(function () {
+    initMap();
+    ajaxPays();
+    console.log("ready");
+});
+
+function initMap() {
+    mymap = L.map('preview').setView([48.864716, 2.349014], 3);
+    L.tileLayer(`https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}`, {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        zoomSnap: 0,
+        zoomDelta: 1,
+        minZoom: 3, 
+        id: 'mapbox.streets',
+        accessToken: token
+    }).addTo(mymap);
+    console.log("initMap")
+}
+
+
+function ajaxPays() {
+    $.ajax({
+        type: 'GET',
+        url: '/phpscripts/retrievePays.php',
+        dataType: 'json',
+        error: function (xhr, status, error) {
+            alert("La récupération des pays a échoué" + error);
+        },
+        success: displayPays
+    });
+}
+
+function displayPays(data) {
+    console.log(data);
+}
+
+function displayInfo(data){
+    
+}
