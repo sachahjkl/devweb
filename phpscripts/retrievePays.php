ae7b384144513196972bb0d1b679f8f2fe59a5c6
sachafroment <sacha.froment@gmail.com>
2019-05-24 18:27:04 +0200

    retrievePays

---
+++ phpscripts/retrievePays.php
@@ -0,0 +1,15 @@
+<?php
+    require("connectBD.php");
+
+    $sql_pays = "SELECT * FROM pays";
+    $result1 = mysqli_query($connection, $sql_pays);
+    $array_result = [];
+    var_dump($result1);
+    $i = 0;
+    while($row1 = mysqli_fetch_array($result1, MYSQLI_ASSOC)){
+        $array_result[] = $row1;
+    }
+
+    $json_result = json_encode($array_result);
+    echo $json_result;
+ ?>
