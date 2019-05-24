6090dd875135241545e6eab58d19d2273c4ca572
sikny <pamartqu@gmail.com>
2019-05-24 17:05:48 +0200

    index changes

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
     <?php include "layouts/head.php" ?>
     <body>
         <?php include "layouts/header.php" ?>
-        <h1>Site Web 24h</h1>
+        <div id="body-content">
+
+        </div>
         <?php include "layouts/footer.php" ?>
         <?php include "layouts/forms/register.php" ?>
     </body>
