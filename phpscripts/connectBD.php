6966ef397bcd63d47fdd86bedb0cd52e8089aff5
sikny <pamartqu@gmail.com>
2019-05-24 14:23:46 +0200

    Script connexion base de données

--- phpscripts/connectBD.php
+++ phpscripts/connectBD.php
@@ -1,8 +1,12 @@
 <?php
-    $host = ;
-    $username = ;
-    $passwd = ;
-    $dbname = ;
+    $host = "localhost";
+    $username = "user1";
+    $passwd = "user1";
+    $dbname = "plsql";
 
-    
+    $connection = mysqli_connect($host, $username, $passwd, $dbname);
+
+    if(!$connection){
+        die("Connection failed : ".mysqli_connect_error());
+    }
  ?>
