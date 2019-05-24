fd582584ab711b7b1baa8a1492242c6a55a94783
sachafroment <sacha.froment@gmail.com>
2019-05-24 17:49:34 +0200

    added

--- layouts/head.php
+++ layouts/head.php
@@ -2,4 +2,5 @@
     <meta charset="utf-8" />
     <link rel="stylesheet" href="style/style.css" />
     <title><?=isset($title) ? $title : "Quefa : Importation de café";?></title>
+    <?=$import?>
 </head>
